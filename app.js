a1bf43c1c71606033b94c38d53c57d7afa81d75a
Leonardo Gomes Ribeiro <leonardometalhead@gmail.com>
2017-03-01 10:08:22 -0300

    using localstorage

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 var myApp = angular.module('myApp', []);
 
 // Define the `PhoneListController` controller on the `SearchMusicApp` module
-myApp.controller('SearchMusicController', function ($scope, $http, SpotifySearchService) {
+myApp.controller('SearchMusicController', function ($scope, $http, $localStorage, SpotifySearchService) {
 	$scope.artists = [];
 	$scope.tracks = [];
 	$scope.search = function (){
 		var searchTerm = $scope.searchText;
-		SpotifySearchService.spotifySearch(searchTerm).then(function(response) {
-				$scope.artists = response.data.artists.items;
-		});
+		if ($localStorage.containsKey(searchTerm)) {
+			$scope.artists = $localStorage.getObject(searchTerm);
+		} else {
+			SpotifySearchService.spotifySearch(searchTerm).then(function(response) {
+					$scope.artists = response.data.artists.items;
+					$localStorage.setObject(searchTerm, $scope.artists);
+			});	
+		}
 	};
 });
