963bc56bbe1b1546bbc9d39ed2a4ab3c40b63ac1
Leonardo Gomes Ribeiro <leonardometalhead@gmail.com>
2017-03-01 09:43:24 -0300

    inject service into app js and removed some references

--- app.js
+++ app.js
@@ -2,15 +2,13 @@
 var myApp = angular.module('myApp', []);
 
 // Define the `PhoneListController` controller on the `SearchMusicApp` module
-myApp.controller('SearchMusicController', function ($scope, $http) {
+myApp.controller('SearchMusicController', function ($scope, $http, SpotifySearchService) {
 	$scope.artists = [];
 	$scope.tracks = [];
 	$scope.search = function (){
 		var searchTerm = $scope.searchText;
-		$http.get("https://api.spotify.com/v1/search?q="+searchTerm+"&type=track,artist&market=US")
-			.then(function(response) {
+		SpotifySearchService.spotifySearch(searchTerm).then(function(response) {
 				$scope.artists = response.data.artists.items;
-				$scope.tracks = response.data.tracks.items;
-		});	
+		});
 	};
 });
