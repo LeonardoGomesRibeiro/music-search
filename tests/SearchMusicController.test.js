65e692b2cef1c55d2fdf5aee512c1336116beffa
Leonardo Gomes Ribeiro <leonardometalhead@gmail.com>
2017-03-01 22:57:30 -0300

    updated tests

--- tests/SearchMusicController.test.js
+++ tests/SearchMusicController.test.js
@@ -16,19 +16,56 @@ describe('myApp', function () {
 			}); 
 		};
 	}));
-
+	
+	var service;
+	var $httpBackend;
+	var $localstorage;
+	beforeEach(inject(function($injector) {
+		service = $injector.get('SpotifySearchService');
+		$httpBackend = $injector.get('$httpBackend');
+		$localstorage = $injector.get('$localstorage');
+		var url = "https://api.spotify.com/v1/search?q=Muse&type=track,artist&market=US";
+		$httpBackend.when('GET', url).respond(["muse album 1"]);
+	}));
+	
 	describe('spotify-service', function () {
 		it('is defined', function () {			
-			 //expect(SpotifySearchService).toBeDefined();
+			 expect(SpotifySearchService).toBeDefined();
 		});	
 		
 		it('will return no results', function () {
 			var controller = new createController();
 			expect(scope).toBeDefined();
 			expect(controller).toBeDefined();
+			scope.searchText='Muse';	
 			scope.search();
 			expect(scope.artists).toBeDefined();
 			expect(scope.artists.length).toBe(0);
+		});
+		
+		it('will return one result', function(){
+			service.spotifySearch('Muse').then(function(response){
+				expect(response.data.length).toEqual(1);
+			});
+			$httpBackend.flush();
+		});
+		
+		it('is defined localstorage', function(){
+			expect($localstorage).toBeDefined();
+		});
+		
+		it('will save string object in localstorage cache', function(){
+			expect($localstorage).toBeDefined();
+			$localstorage.set('key','test');
+			expect($localstorage.containsKey('key')).toBeDefined();
+			expect($localstorage.get('key')).toEqual('test');
+		});
+		
+		it('will save json object in localstorage cache', function(){
+			expect($localstorage).toBeDefined();
+			$localstorage.setObject('keyObj', '{[{name:"muse"}]}');
+			expect($localstorage.containsKey('keyObj')).toBeDefined();
+			expect($localstorage.getObject('keyObj')).toEqual('{[{name:"muse"}]}');
 		});
 	});
 });
